6f7e0066499b671e0e9ace48f56e0ff689059e8b
jgordowolox <juanmartin.gordo@wolox.com.ar>
2021-04-12 19:04:01 -0300

    Added localization for Spanish and English languages

--- Wbooks/Library/LibraryController.swift
+++ Wbooks/Library/LibraryController.swift
@@ -12,7 +12,6 @@ final class LibraryController: UIViewController, UITableViewDelegate, UITableVie
     
     // MARK: Properties
     private lazy var libraryView: LibraryView = LibraryView()
-    private let viewTitle: String = "Library"
     private let booksArray : [Book] = [
         Book(cover: UIImage(named: "BookCover1")!,
              title: "A Little Bird Told Me",
@@ -46,7 +45,7 @@ final class LibraryController: UIViewController, UITableViewDelegate, UITableVie
     
     /// Sets up the navigation bar buttons for this specific view
     private func setupNavBar() {
-        navigationItem.title = viewTitle.uppercased()
+        navigationItem.title = NSLocalizedString("LIBRARY_VIEW_TITLE", comment: "").uppercased()
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             image: UIImage(named: "SearchButton"),
             style: .plain,
--- Wbooks/Main/MainView.swift
+++ Wbooks/Main/MainView.swift
@@ -11,6 +11,25 @@ import UIKit
 final class MainView: NibView {
     
     // MARK: Properties
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userLabel: UILabel! {
+        didSet {
+            userLabel.text = NSLocalizedString("USERNAME_LABEL", comment: "")
+        }
+    }
+    @IBOutlet weak var userInput: UITextField! {
+        didSet {
+            userInput.placeholder = NSLocalizedString("USERNAME_TEXT_FIELD_PLACEHOLDER", comment: "")
+        }
+    }
+    @IBOutlet weak var passwordLabel: UILabel! {
+        didSet {
+            passwordLabel.text = NSLocalizedString("PASSWORD_LABEL", comment: "")
+        }
+    }
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.setTitle(NSLocalizedString("LOGIN_BUTTON", comment: ""), for: .normal)
+        }
+    }
     
 }
