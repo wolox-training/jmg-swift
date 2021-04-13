8e327f2e071a59f04c65a205ace45b48406ae4c5
jgordowolox <juanmartin.gordo@wolox.com.ar>
2021-04-13 17:09:20 -0300

    Added localization for tab bar button titles

--- Wbooks/Navigation/TabBarController.swift
+++ Wbooks/Navigation/TabBarController.swift
@@ -24,31 +24,31 @@ final class TabBarController: UITabBarController {
         viewControllers = [library, wishlist, addNew, rentals, settings]
                 
         library.tabBarItem = UITabBarItem()
-        library.tabBarItem.title = "Library"
+        library.tabBarItem.title = NSLocalizedString("TAB_BAR.LIBRARY_BUTTON_TITLE", comment: "Title under the Library button in the tab bar")
         library.tabBarItem.image = UIImage(named: "LibraryButton")
         library.tabBarItem.selectedImage = UIImage(named: "LibraryButtonActive")
         library.tabBarItem.tag = 0
         
         wishlist.tabBarItem = UITabBarItem()
-        wishlist.tabBarItem.title = "Wishlist"
+        wishlist.tabBarItem.title = NSLocalizedString("TAB_BAR.WISHLIST_BUTTON_TITLE", comment: "Title under the Wishlist button in the tab bar")
         wishlist.tabBarItem.image = UIImage(named: "WishlistButton")
         wishlist.tabBarItem.selectedImage = UIImage(named: "WishlistButtonActive")
         wishlist.tabBarItem.tag = 1
         
         addNew.tabBarItem = UITabBarItem()
-        addNew.tabBarItem.title = "Add New"
+        addNew.tabBarItem.title = NSLocalizedString("TAB_BAR.ADDNEW_BUTTON_TITLE", comment: "Title under the Add New button in the tab bar")
         addNew.tabBarItem.image = UIImage(named: "AddNewButton")
         addNew.tabBarItem.selectedImage = UIImage(named: "AddNewButtonActive")
         addNew.tabBarItem.tag = 2
         
         rentals.tabBarItem = UITabBarItem()
-        rentals.tabBarItem.title = "Rentals"
+        rentals.tabBarItem.title = NSLocalizedString("TAB_BAR.RENTALS_BUTTON_TITLE", comment: "Title under the Rentals button in the tab bar")
         rentals.tabBarItem.image = UIImage(named: "RentalsButton")
         rentals.tabBarItem.selectedImage = UIImage(named: "RentalsButtonActive")
         rentals.tabBarItem.tag = 3
         
         settings.tabBarItem = UITabBarItem()
-        settings.tabBarItem.title = "Settings"
+        settings.tabBarItem.title = NSLocalizedString("TAB_BAR.SETTINGS_BUTTON_TITLE", comment: "Title under the Settings button in the tab bar")
         settings.tabBarItem.image = UIImage(named: "SettingsButton")
         settings.tabBarItem.selectedImage = UIImage(named: "SettingsButtonActive")
         settings.tabBarItem.tag = 4
