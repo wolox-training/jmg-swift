547d898a0fefd2009ecdece2b9cb1508cc19d302
jgordowolox <juanmartin.gordo@wolox.com.ar>
2021-04-26 15:56:52 -0300

    Created comment cell view

---
+++ Wbooks/Detail/Comments Section/CommentCell.swift
@@ -0,0 +1,12 @@
+//
+//  CommentCell.swift
+//  Wbooks
+//
+//  Created by Juan Martín Gordo on 26/04/2021.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+}
---
+++ Wbooks/Detail/Comments Section/CommentCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CommentCellViewModel.swift
+//  Wbooks
+//
+//  Created by Juan Martín Gordo on 26/04/2021.
+//
+
+import UIKit
+
+struct CommentCellViewModel {
+    
+}
